75587d184f519c110172a6c0f55598d45feb943a
Haileab Wolde <womahaileab1428@gmail.com>
2022-11-29 08:57:28 -0800

    printing 3 times a string stored

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+first_eightchar = str[0:9] #to print the first eight characters
+print(str + str + str) #printing 3 times a string
+print(first_eightchar)
