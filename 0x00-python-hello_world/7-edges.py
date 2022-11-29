9f9d0a0996e19c2011a4dc24964679a60fb7c3e2
Haileab Wolde <womahaileab1428@gmail.com>
2022-11-29 09:32:11 -0800

    holding characters

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3] #to hold the first 3 characters
+word_last_2 = word[0:2] #to hold the first 2 characters
+middle_word = word[1:8] #to hold the first 8 characters
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
