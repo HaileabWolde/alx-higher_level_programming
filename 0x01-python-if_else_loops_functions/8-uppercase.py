c4b929fc828f09b5efb7d0b8542f8a72ef10dbb0
Haileab Wolde <womahaileab1428@gmail.com>
2022-12-01 04:19:58 -0800

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(str):
         if (i == " "):
             result += " "
         elif (97 <= ord(i) <= 122):  # to change into uppercase
-            result += chr(ord(i) - 32)  # ASCII ORDER 
+            result += chr(ord(i) - 32)
         else:
             result += chr(ord(i))
     print("{}".format(result))
