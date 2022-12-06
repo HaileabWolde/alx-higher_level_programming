42b3f73066da7a22a88d3b387bbe19aaae4b71e2
Haileab Wolde <womahaileab1428@gmail.com>
2022-12-06 11:24:53 -0800

    list and tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        n = len(my_list)
-        for i in range(0, n):
-            print("{}".format(my_list[i]))
+
+def print_list_integer(my_list=[]):
+    n = len(my_list)
+    for i in range(0, n):
+        print("{:d}".format(my_list[i]))
