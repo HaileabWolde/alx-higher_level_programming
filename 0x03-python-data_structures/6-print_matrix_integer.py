b33d6deb336af22b3f1c48eeae86eb0c024e4b58
Haileab Wolde <womahaileab1428@gmail.com>
2022-12-07 04:45:00 -0800

    multidimensional

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
+"""A function that prints a matrix of integers"""
+
+
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:  # to print multi-dimension array
-        for j in i:
-            if j != 0:
+    for index in range(len(matrix)):
+        for i in range(len(matrix[index])):
+            if i != 0:
                 print(" ", end='')
-            print("{:d}".format(j), end=" ")
+            print("{:d}".format(matrix[index][i]), end='')
         print()
