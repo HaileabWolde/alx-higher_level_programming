5c38f7e2319c86b6e23fbd560d432da241cfa063
Haileab Wolde <womahaileab1428@gmail.com>
2022-12-07 04:42:48 -0800

    multi-dimensional

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:  # to print multi-dimension array
         for j in i:
+            if j != 0:
+                print(" ", end='')
             print("{:d}".format(j), end=" ")
         print()
