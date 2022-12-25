517bffc463658401496317af71b5520a2a7cf1b5
Haileab Wolde <womahaileab1428@gmail.com>
2022-12-25 07:24:09 -0800

    classes and objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-#1-square.py by Haileab Woma
+# 1-square.py by Haileab Woma
 """Defines a square"""
 
+
 class Square:
+
     """Represents a square"""
-    
+
     def __init__(self, size):
 
         """Initilizing this square class
-            Args: size  - represents the size of the square defined
-            """
+                Args: size  - represents the size of the square defined
+        """
         self.__size = size
