17458904e8dde92f9e928e05890a00b4191ab63f
Haileab Wolde <womahaileab1428@gmail.com>
2023-01-11 12:24:09 -0800

    inhertance question number 4

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+"""checking an object is an instance of class or not"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checking an object is an instance or not
+    Args:
+        obj: object
+        a_class: the class it bleongs
+    """
+    if isinstance(obj, a_class):
+        return True
+
+    return False
