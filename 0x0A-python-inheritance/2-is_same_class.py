20545e4e139e607102d8606d8131ac6f51c802fe
Haileab Wolde <womahaileab1428@gmail.com>
2023-01-11 11:52:34 -0800

    inhertance question 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""It reutrns true if the object is exactly or else false"""
+
+
+def is_same_class(obj, a_class):
+    """Initalizing of this class
+    Args:
+        obj: repersents the objected number
+        a_class: the class that is to be checked
+    """
+    if isinstance(obj, a_class):
+        return True
+
+    else:
+        return False
