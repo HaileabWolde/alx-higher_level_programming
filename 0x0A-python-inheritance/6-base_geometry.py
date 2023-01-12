35b775427f67ecd1b1a7e1dcc04029773dbde0ca
Haileab Wolde <womahaileab1428@gmail.com>
2023-01-12 00:14:44 -0800

    raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A classs that raises execption"""
+
+
+class BaseGeometry:
+    """A class that raise exceptions"""
+
+    def area(self):
+        """a method that intialize itself"""
+        raise Exception("area() is not implemented")
